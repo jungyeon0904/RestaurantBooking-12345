399eccbc6e3a3dca3ca7ac1d6fff352f98f1fb4a
Jungyeon Kim <wjddus0904@naver.com>
2025-05-07 17:05:38 +0900

    Test case refactoring with gmock

--- RestaurantBooking/testable_sms_sender.cpp
+++ RestaurantBooking/testable_sms_sender.cpp
@@ -1,17 +1,8 @@
+#include "gmock/gmock.h"
 #include <iostream>
 #include "sms_sender.cpp"
 
 class TestableSmsSender : public SmsSender {
 public:
-	void send(Schedule* schedule) override {
-		std::cout << "테스트용 SmsSender class의 send 메서드 실행\n";;
-		sendMethodIsCalled = true;
-	}
-
-	bool isSendMethodIsCalled() {
-		return sendMethodIsCalled;
-	}
-
-private:
-	bool sendMethodIsCalled;
+	MOCK_METHOD(void, send, (Schedule*), (override));
 };
