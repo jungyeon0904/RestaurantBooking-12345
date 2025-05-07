4770e37ad777fd9a314a8d29f560d474cbe69297
Jungyeon Kim <wjddus0904@naver.com>
2025-05-07 15:14:26 +0900

    First commit

---
+++ RestaurantBooking/mail_sender.cpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "schedule.cpp"
+
+class MailSender {
+public:
+	void sendMail(Schedule* schedule) {
+		//send mail
+	}
+};
---
+++ RestaurantBooking/sms_sender.cpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "schedule.cpp"
+
+class SmsSender {
+public:
+	void send(Schedule* schedule) {
+		//send message to PhoneNumber
+	}
+};
